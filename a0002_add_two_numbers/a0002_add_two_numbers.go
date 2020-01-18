e2816cb79053bd1ac5b1ac3c9a79b05b723eae46
Daniel <qiuzhanghua@icloud.com>
2020-01-18 22:26:31 +0800

    Another method for add two numbers

--- a0002_add_two_numbers/a0002_add_two_numbers.go
+++ a0002_add_two_numbers/a0002_add_two_numbers.go
@@ -7,61 +7,23 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	cur1 := l1
-	cur2 := l2
-	var head, cur *ListNode
-	sum, carry := 0, 0
-	for cur1 != nil && cur2 != nil {
-		sum = (cur1.Val + cur2.Val + carry) % 10
-		carry = (cur1.Val + cur2.Val + carry) / 10
+	result := &ListNode{}
+	current := result
+	eachSums := 0
 
-		if head == nil {
-			head = &ListNode{sum, nil}
-			cur = head
-		} else {
-			cur.Next = &ListNode{sum, nil}
-			cur = cur.Next
+	for l1 != nil || l2 != nil || eachSums != 0 {
+		if l1 != nil {
+			eachSums += l1.Val
+			l1 = l1.Next
 		}
-
-		cur1 = cur1.Next
-		cur2 = cur2.Next
-	}
-
-	// Loop over left over l1
-	for cur1 != nil {
-		sum = (cur1.Val + carry) % 10
-		carry = (cur1.Val + carry) / 10
-
-		if head == nil {
-			head = &ListNode{sum, nil}
-			cur = head
-		} else {
-			cur.Next = &ListNode{sum, nil}
-			cur = cur.Next
+		if l2 != nil {
+			eachSums += l2.Val
+			l2 = l2.Next
 		}
-		cur1 = cur1.Next
+		current.Next = &ListNode{Val: eachSums % 10}
+		eachSums /= 10
+		current = current.Next
 	}
 
-	// Loop over left over l2
-	for cur2 != nil {
-		sum = (cur2.Val + carry) % 10
-		carry = (cur2.Val + carry) / 10
-
-		if head == nil {
-			head = &ListNode{sum, nil}
-			cur = head
-		} else {
-			cur.Next = &ListNode{sum, nil}
-			cur = cur.Next
-		}
-		cur2 = cur2.Next
-	}
-
-	// Take in any carry
-	if carry != 0 {
-		cur.Next = &ListNode{carry, nil}
-		cur = cur.Next
-	}
-	return head
-
+	return result.Next
 }
